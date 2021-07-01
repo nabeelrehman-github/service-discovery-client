17e81670f345556c766eea58a23fff08891cd159
Nabeel Rehman <Nabeel Rehman@192.168.10.10>
2021-07-01 13:05:37 +0500

    fix

--- src/main/java/com/servicediscvoery/ServiceDiscoveryClientApplication.java
+++ src/main/java/com/servicediscvoery/ServiceDiscoveryClientApplication.java
@@ -3,12 +3,12 @@ package com.servicediscvoery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.client.RestTemplate;
 
 @EnableDiscoveryClient
 @EnableScheduling
-@ComponentScan("com.servicediscvoery.*")
 @SpringBootApplication
 public class ServiceDiscoveryClientApplication {
 
@@ -16,9 +16,9 @@ public class ServiceDiscoveryClientApplication {
 		SpringApplication.run(ServiceDiscoveryClientApplication.class, args);
 	}
 
-//	@Bean
-//	public RestTemplate restTemplate() {
-//		return new RestTemplate();
-//	}
+	@Bean
+	public RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
 
 }
